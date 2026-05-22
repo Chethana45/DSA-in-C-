2f6db56fca48249cab9a0a28ab7fbc98161d9296
AdusumalliChethanaSri <chethanasriadusumalli@gmail.com>
2026-05-22 20:03:22 +0530

    Create armstrong.cpp

---
+++ armstrong.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, original, sum = 0;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    original = num;
+
+    while (num != 0) {
+        int digit = num % 10;
+        sum = sum + (digit * digit * digit);
+        num = num / 10;
+    }
+
+    if (sum == original) {
+        cout << "Armstrong Number";
+    } else {
+        cout << "Not an Armstrong Number";
+    }
+
+    return 0;
+}
