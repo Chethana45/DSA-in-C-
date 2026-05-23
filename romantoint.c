122d985f474e28e173bbfdbb245bdead279652d0
AdusumalliChethanaSri <chethanasriadusumalli@gmail.com>
2026-05-23 21:54:14 +0530

    Create romantoint.c

---
+++ romantoint.c
@@ -0,0 +1,36 @@
+int romanToInt(char* s) {
+    int ans=0;
+    int num=0;
+    for(int i= strlen(s)-1;i>=0;i--){
+        switch(s[i]){
+            case 'I':
+            num =1;
+            break;
+            case 'V':
+            num=5;
+            break;
+            case 'X':
+            num = 10;
+            break;
+            case 'L':
+            num =50;
+            break;
+            case 'C':
+            num= 100;
+            break;
+            case 'D':
+            num=500;
+            break;
+            case 'M':
+            num =1000;
+            break; }
+            if(4*num<ans)
+                ans-=num;
+             else
+                ans+=num;
+    }
+            return ans;
+
+        
+    }
+    
