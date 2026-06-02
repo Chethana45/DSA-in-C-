a62d76093b3d58badeaf5d83734ac22143aeb1b2
Chethana45 <chethanasriadusumalli@gmail.com>
2026-06-02 16:49:12 +0530

    Create c21.cpp

---
+++ c21.cpp
@@ -0,0 +1,16 @@
+int main(){
+    int n=5;
+
+
+    for(int i=0;i<n;i++){
+        for(int j=n-1;j>i;j--){
+            cout<<"*";
+        }
+        for(int j=0;j<2*i+2;j++){
+            cout<<" ";
+        }
+        for(int j=n-1;j>i;j--){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
