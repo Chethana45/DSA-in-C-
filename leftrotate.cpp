009a4a5e068d7b28987645a97df9e76dafec77a1
Chethana45 <chethanasriadusumalli@gmail.com>
2026-06-07 19:55:35 +0530

    Add rotateArrayByOne method to rotate array
    Implement a method to rotate an array by one position.

---
+++ leftrotate.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void rotateArrayByOne(vector<int>& nums) {
+        int temp=nums[0];
+        for(int i=1;i<n;i++){
+            nums[i-1]=nums[i];
+        }
+        nums[n-1]=temp;
+        return nums;
+
+    }
+};
