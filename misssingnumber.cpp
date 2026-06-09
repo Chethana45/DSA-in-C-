f09d44408a318ac08f325a20d73ac4762705b313
Chethana45 <chethanasriadusumalli@gmail.com>
2026-06-09 15:37:36 +0530

    Create misssingnumber.cpp

---
+++ misssingnumber.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int sum=(n*(n+1))/2;
+        int actsum=0;
+        for(int i=0;i<n;i++){
+            actsum+=nums[i];
+        }
+return sum-actsum;
+        
+    }
+};
